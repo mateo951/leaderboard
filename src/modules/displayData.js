de7b7766e59ebee44396d240c733970bf7e2c32e
Mateo Villagómez <mateovillagomez1995@gmail.com>
2021-09-03 09:52:36 -0500

    Add required changes

--- src/modules/displayData.js
+++ src/modules/displayData.js
@@ -6,12 +6,6 @@ const displayData = (data) => {
   result.forEach((item) => {
     listItems.innerHTML += `<li class="list-group-item">${item.user}: <span class="floatRight badge badge-primary badge-pill">${item.score}</span></li>`;
   });
-
-//   list.innerHTML = '';
-//   scores.forEach((score) => {
-//     list.innerHTML += `<li>${score[0]}: ${score[1]}</li>`;
-//   });
-//   if (scores.length > 5) { list.classList.add('scroll'); }
 };
 
 export default displayData;
